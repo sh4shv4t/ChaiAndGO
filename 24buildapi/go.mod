19dea6a0ffdc37e1e74341c86285632fbc87db8c
sh4shv4t <shashvat.k.singh.16@gmail.com>
2026-02-21 15:57:13 +0530

    Learning about building Go apis

---
+++ 24buildapi/go.mod
@@ -0,0 +1,5 @@
+module github.com/sh4shv4t/buildapi
+
+go 1.25.6
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ 24buildapi/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// model for course - file
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+var courses []Course //fake db
+
+// middleware, helper - file
+func (c *Course) isEmpty() bool {
+	return c.CourseName == ""
+}
+
+func main() {
+
+}
+
+//controllers - file
+
+//server home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API development by LCO - sh4shv4t</h1>"))
+}
+
+func getAllCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all Courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grabbing id
+	params := mux.Vars(r)
+
+	//find matching id in the courses and return
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("No course found with given id %s", params["id"])})
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+		return
+	}
+
+	//what if -{}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.isEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	//generate uniqu id, string
+	//append new course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
